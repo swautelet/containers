1712643983ff02d4fe50a793b2b4d747f140402e
swautele <swautle@student.s19.com>
2022-12-15 15:15:57 +0100

    save

---
+++ include/print_map.hpp
@@ -0,0 +1,29 @@
+#ifndef PRINT_MAP_HPP
+# define PRINT_MAP_HPP
+
+#include "Map.hpp"
+
+// template <class A, class B>
+void    print_node(map_node<int, int >* x){
+	if (!x){
+		std::cout << "this is not a node but a null pointer " << std::endl;
+		return ;
+	}
+	else if (x->getChild_l() && x->getChild_r())
+		std::cout << x->First() << "	this node has two child : " << x->getChild_l()->First() << " and " << x->getChild_r()->First() << std::endl;
+	else if (x->getChild_r())
+		std::cout << x->First() << "	this node has only a right child : " << x->getChild_r()->First() << std::endl;
+	else if (x->getChild_l())
+		std::cout << x->First() << "	this node has only a left child : " << x->getChild_l()->First() << std::endl;
+	else 
+		std::cout << x->First() << "	this node has no child : " << std::endl;
+	// std::cout << " first : " << x->First() << " second : " << x->Second() << std::endl;
+	if (x->getChild_l())
+		print_node(x->getChild_l());
+	if (x->getChild_r())
+		print_node(x->getChild_r());
+}
+
+
+
+#endif
